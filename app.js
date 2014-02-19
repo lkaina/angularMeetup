20c6813a23f650bec543d54decbd5961d2666486
Leighton Kaina <leighton.kaina@gmail.com>
2014-02-18 19:27:22 -0800

    Add flickr ajax request for photos.

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 angular.module('myApp', [])
-.controller('HomeController', function($scope) {
-  
+.controller('HomeController', function($scope, $http) {
+  $scope.user = {
+    name: 'Leighton'
+  };
+  var apiKey = '6bf0a7b18ff0c5e480fd8dff6ea9bd9e';
+  $scope.getPhotoUrl = function(photo) {
+    return "http://farm"+photo.farm+
+    ".static.flickr.com/"+photo.server+
+    "/"+photo.id+"_"+photo.secret+".jpg";
+  };
+
+  $http({
+    method:'GET',
+    url: 'http://api.flickr.com/services/rest',
+    params: {
+      //Flickr API parameters
+      method: 'flickr.interestingness.getList',
+      api_key: apiKey,
+      format: 'json',
+      per_page: 3,
+      nojsoncallback: 1
+    }
+  }).then(function(data) {
+    $scope.photos = data.data;
+  });
 });
