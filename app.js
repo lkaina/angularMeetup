1f2b93f1bb9661beef6eef08f3f9d4127e69cf16
Leighton Kaina <leighton.kaina@gmail.com>
2014-02-18 20:10:07 -0800

    Use provider for modularity.  Add router to handle templates.

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
-angular.module('myApp', [])
-.controller('HomeController', function($scope, $http) {
+angular.module('myApp', ['ngRoute']) //module dependences
+.provider('Flickr', function() {
+  var apiKey = '6bf0a7b18ff0c5e480fd8dff6ea9bd9e';
+  this.setApiKey = function(key) {
+  apikey = key || apiKey;
+ };
+ this.$get = function($http) { //why get here?
+  this.getPhotos = function(){
+    return $http({
+      method:'GET',
+      url: 'http://api.flickr.com/services/rest',
+      params: {
+        //Flickr API parameters
+        method: 'flickr.interestingness.getList',
+        api_key: apiKey,
+        format: 'json',
+        nojsoncallback: 1
+      }
+    }).then(function(data) {
+      return data.data.photos.photo;
+    });
+  }
+  return this;
+ };
+})
+.config(function(FlickrProvider) {  //FlickrProvider available only during config, automatically added by angular
+  FlickrProvider.setApiKey('6bf0a7b18ff0c5e480fd8dff6ea9bd9e');
+})
+.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'home.html',
+    controller: 'HomeController'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+})
+.controller('HomeController', function($scope, Flickr) {
   $scope.user = {
     name: 'Leighton'
   };
-  var apiKey = '6bf0a7b18ff0c5e480fd8dff6ea9bd9e';
   $scope.getPhotoUrl = function(photo) {
     return "http://farm"+photo.farm+".static.flickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+".jpg";
   };
-
-  $http({
-    method:'GET',
-    url: 'http://api.flickr.com/services/rest',
-    params: {
-      //Flickr API parameters
-      method: 'flickr.interestingness.getList',
-      api_key: apiKey,
-      format: 'json',
-      nojsoncallback: 1
-    }
-  }).then(function(data) {
-    $scope.photos = data.data.photos.photo;
-  });
+  Flickr.getPhotos().then(function(data) {
+    $scope.photos = data;
+  })
 });
